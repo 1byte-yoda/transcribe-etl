a1d810ddd7c77974080941642e53669f35b1dfaa
Mark Caneda <us66@klavenessdigital.com>
2022-11-19 23:40:31 +0800

    Moved interval functions

--- transcribe_etl/transform/helper.py
+++ transcribe_etl/transform/helper.py
@@ -3,8 +3,7 @@ from typing import Tuple
 
 
 def split_interval(interval: str) -> Tuple[str, str]:
-    matched_groups = re.findall(r"(\d{2}:\d{2}:\d{1,2}\.*\d{0,3})\s(\d{2}:\d{2}:\d{1,2}\.*\d{0,3})",
-                                interval.strip())
+    matched_groups = re.findall(r"(\d{2}:\d{2}:\d{1,2}\.*\d{0,3})\s(\d{2}:\d{2}:\d{1,2}\.*\d{0,3})", interval.strip())
     start, end = matched_groups[0] if len(matched_groups) > 0 else ("", "")
     return start, end
 
@@ -16,8 +15,8 @@ def convert_duration_to_millisecond(duration: str) -> int:
     return ((hours * 60 * 60) + (minutes * 60 + seconds)) * 1000 + milliseconds
 
 
-def convert_interval_to_milliseconds(cls, interval: str) -> Tuple[int, int]:
-    start, end = cls.split_interval(interval=interval)
-    start_ms = cls.convert_duration_to_millisecond(duration=start)
-    end_ms = cls.convert_duration_to_millisecond(duration=end)
+def convert_interval_to_milliseconds(interval: str) -> Tuple[int, int]:
+    start, end = split_interval(interval=interval)
+    start_ms = convert_duration_to_millisecond(duration=start)
+    end_ms = convert_duration_to_millisecond(duration=end)
     return start_ms, end_ms
