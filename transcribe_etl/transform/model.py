cdff6acfee253eb469b2979a08512bb59c3d1792
Mark Caneda <us66@klavenessdigital.com>
2022-11-19 23:39:26 +0800

    Added models for Transcription Package

--- transcribe_etl/transform/model.py
+++ transcribe_etl/transform/model.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+from typing import Optional, List
 
 from dataclasses_json import DataClassJsonMixin
 
@@ -9,3 +10,25 @@ class TxData(DataClassJsonMixin):
     text: str
     start: int
     end: int
+
+
+@dataclass(frozen=True)
+class Speaker:
+    user_id: str
+    email: str
+    gender: str
+    native_language: str
+
+
+@dataclass(frozen=True)
+class MetaData(DataClassJsonMixin):
+    audio_file_name: str
+    audio_duration: float
+    corpus_code: Optional[str]
+    speaker_id: Speaker
+
+
+@dataclass(frozen=True)
+class TranscriptionPackage:
+    tx_data: List[TxData]
+    metadata: MetaData
